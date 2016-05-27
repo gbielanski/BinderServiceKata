7a76f9d402e8ab9b398a895fc1b96ff1bffc49ac
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2016-05-27 16:45:57 +0200

    Initial commit

---
+++ app/src/main/java/pl/kata/bielanski/binderservicekata/MainActivity.java
@@ -0,0 +1,18 @@
+package pl.kata.bielanski.binderservicekata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void onButtonClick(View v){
+
+	}
+}
